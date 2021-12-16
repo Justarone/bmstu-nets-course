8b16fa7a00fc9a3fff947c7c4c009bd3b18ef9d8
Perestoronin Pavel <p_perestoronin@mail.ru>
2021-12-16 21:55:58 +0300

    Change text serialization to binary one

--- common/include/message.h
+++ common/include/message.h
@@ -1,6 +1,6 @@
 #pragma once
-#include <boost/archive/text_iarchive.hpp>
-#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
 #include <boost/serialization/string.hpp>
 #include <boost/serialization/vector.hpp>
 #include <cstdint>
